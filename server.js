96d0c33d06ccd5d3d72b2b6bd493134544af4eb4
jdolitsky <jdolitsky@mediatemple.net>
2013-08-24 13:44:01 -0700

    push

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var express = require('express');
 var engine = require('ejs-locals');
 var mongoose = require('mongoose');
 
-
 // connect to MongoDB
 var db = 'bootcamp';
 mongoose.connect('mongodb://localhost/'+db);
@@ -24,9 +23,52 @@ app.use(express.bodyParser());
 // port that server will listen on
 var port = 3000;
 
+// create user model 
+var User = mongoose.model('User', {
+	username: String,
+	password: String,
+	image: String,
+	bio: String,
+	friends: Array
+});
+
 // start listening...
 app.listen(port);
 console.log('Express server listening on port '+port);
+
+app.get('/newuser/:username', function (req, res) {
+	var username = req.params.username;
+	var newUser = new User({
+		username: username,
+		password: "thisismypass",
+		bio: "Just a regular guy",
+		image: "https://0.gravatar.com/avatar/79943d1a567466f80e313d18728ea205?d=https%3A%2F%2Fidenticons.github.com%2F42a78e833c8827c576df9398f411616f.png",
+		friends: []
+	}).save(function (err){
+		console.log('New user '+username+' created!');
+		res.send('welcome '+username);
+	});
+	
+});
+
+app.get('/allusers',function (req, res){
+
+	var query = {};
+	User.find(query, function (err, users) {
+
+		res.send(users);
+
+	});
+});
+
+app.get('/updateuser', function (req, res){
+	var query = {username: "frank"};
+	var changes = {$push: {friends: "friend1" } };
+	User.update(query, changes, function (err, users) {
+
+		res.send('updated!');
+	});
+});
 
 // root route (response for http://localhost:3000/)
 app.get('/', function (req, res) {
